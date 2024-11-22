bfd1c66d01dd3aa93e8bbee6cb83a7d07c915436
jetswag1 <jetswag1013@gmail.com>
2024-11-22 01:09:07 -0500

    Added functionality for transforming address to direction

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import puppeteer from "puppeteer";
+
 const XLSX = require("xlsx");
 
 // Load excel file with addresses
@@ -12,4 +14,40 @@ const data = XLSX.utils.sheet_to_json(sheet);
 
 data.forEach((row) => {
   console.log(row.Name + " " + row.Address + "\n");
+  lonLat = getLongLat(JSON.stringify(row.Address));
 });
+
+// retreives longitude and latitude
+async function getLongLat(address) {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL.
+  await page.goto("https://www.latlong.net/convert-address-to-lat-long.html");
+
+  // Set screen size.
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  // Type into address box.
+  await page.locator(".width70").fill(address);
+
+  // Wait and click on find button.
+  await page.locator(".btnfind").click();
+
+  // wait for the .lat and .lng field to load
+  await page.waitForSelector(".lat");
+
+  // Record the values
+  latitude = await page.$eval(".lat", (input) => input.value);
+  longitude = await page.$eval(".lng", (input) => input.value);
+
+  // Print the Longitude and Latitude.
+  console.log(
+    "The Latitude is $f and the latitude is %f\n.",
+    latitude,
+    longitude,
+  );
+
+  await browser.close();
+}
